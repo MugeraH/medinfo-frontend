457556ef140e6fbf7cd699cc76e7c2fef9abfedd
MugeraH <bwosihughes24@gmail.com>
2021-06-10 00:58:56 +0300

    update authserices

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+const loginUrl = 'http://localhost:8000/auth/login';
+const logoutUrl = 'http://localhost:8000/auth/logout';
+const registerUrl = 'http://localhost:8000/auth/register';
+const userUrl = 'http://localhost:8000/auth/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  userLogin(data: { username: string; password: string }) {
+    return this.http.post(loginUrl, data);
+  }
+
+  userRegister(data: { username: string; email: string; password: string }) {
+    return this.http.post(registerUrl, data);
+  }
+
+  getUser() {
+    return this.http.get(userUrl, { withCredentials: true });
+  }
+
+  logout() {
+    return this.http.post(logoutUrl, {}, { withCredentials: true });
+  }
 }
